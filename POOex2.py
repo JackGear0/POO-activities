7302aebd3ee7281d7719c217d9da4afcfb84e615
Leo <65366000+JackGear0@users.noreply.github.com>
2020-11-07 15:46:18 -0300

    Add files via upload

---
+++ POOex2.py
@@ -0,0 +1,29 @@
+def Dec(x):
+    return f'{x: >7}'
+
+
+def Oct(x):
+    d = oct(x)[2:]
+    return f'{d: >6}'
+
+
+def Hex(x):
+    c = hex(x)[2:]
+    return f'{c: >12}'
+
+
+def Bin(x):
+    b = bin(x)[2:]
+    return f'{b: >7}'
+
+
+def Print_Table(n):
+    print("Decimal\tOctal\tHexadecimal\tBinario\n"
+          "-------\t------\t-----------\t-------\t")
+    while n < 256:
+        print(Dec(n), Oct(n), Hex(n), Bin(n),)
+        n += 1
+
+
+a = 0
+Print_Table(a)
