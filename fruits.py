6dc7179829eadc050b5b92c673dc21abc797bb87
Leo <65366000+JackGear0@users.noreply.github.com>
2020-11-07 16:33:34 -0300

    Add files via upload

---
+++ fruits.py
@@ -0,0 +1,12 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']  # 1 question
+print(len(fruits))  # 2 question
+print(fruits.count('apple'))  # 3 question
+print(fruits.index('banana'))  # 4 question
+fruits.reverse()
+print(fruits)  # 5 question
+fruits.append('grape')
+print(fruits)  # 6 question
+fruits.sort()
+print(fruits)  # 7 question
+fruits.pop()
+print(fruits)  # 8 question
