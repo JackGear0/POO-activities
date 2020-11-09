8391c26f2bf1a43666789fc7075d46a01da918f7
Leo <65366000+JackGear0@users.noreply.github.com>
2020-11-09 16:21:53 -0300

    Update POOex2.py
    Fazer uma sequencia de impressão onde sejam mostras as representações  decimal, octal, hexadecimal dos valores inteiros de 0 a 225 (que são os valores que um byte pode representar).
    Verificar o contéudo dos especificadores de representação no contexto do comando print.
    Cria para cada representação uma função especifica (printDecimal, printOctal, printHexadecimal, printBinario).
    Incluir no código uma funcionalidade imprimirTabela() que executar um laço para imprimir cada linha
    do relatório.

--- POOex2.py
+++ POOex2.py
@@ -1,29 +1,29 @@
-def Dec(x):
+def printDecimal(x):
     return f'{x: >7}'
 
 
-def Oct(x):
+def printOctal(x):
     d = oct(x)[2:]
     return f'{d: >6}'
 
 
-def Hex(x):
+def printHexadecimal(x):
     c = hex(x)[2:]
     return f'{c: >12}'
 
 
-def Bin(x):
+def printBinario(x):
     b = bin(x)[2:]
     return f'{b: >7}'
 
 
-def Print_Table(n):
+def imprimirTabela(n):
     print("Decimal\tOctal\tHexadecimal\tBinario\n"
           "-------\t------\t-----------\t-------\t")
     while n < 256:
-        print(Dec(n), Oct(n), Hex(n), Bin(n))
+        print(printDecimal(n), printOctal(n), printHexadecimal(n), printBinario(n))
         n += 1
 
 
 a = 0
-Print_Table(a)
+imprimirTabela(a)
